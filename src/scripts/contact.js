694bb02a93862c295fe877e86bb988868586f1e1
João Vitor Santana <94786847+jvs-dev@users.noreply.github.com>
2025-08-07 15:11:41 -0300

    add form fix

--- src/scripts/contact.js
+++ src/scripts/contact.js
@@ -1,6 +1,19 @@
 const contactOf = document.getElementById("contactOf")
+const contactForm = document.getElementById("contactForm")
+
 contactOf.onchange = function () {
     if (contactOf.value != "") {
         contactOf.style.color = "#000"
     }
+}
+
+contactForm.onsubmit = function (event) {
+    event.preventDefault()
+    alert("Formulario enviado com sucesso")    
+    let allInputs = document.querySelectorAll("#contactForm input")
+    let message = document.getElementById("message")
+    allInputs.forEach(element => {
+        element.value = ""
+        message.value = ""
+    });
 }
